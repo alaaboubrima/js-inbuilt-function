6b620cdabecd844f05b50628d49fdf8bec4e160b
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2022-08-08 20:02:40 +0100

    Update SquaredIntegers.js

--- SquaredIntegers.js
+++ SquaredIntegers.js
@@ -3,3 +3,4 @@ const squareList = arr => {
 };
 //This function should return a new array containing the squares of only the positive integers 
 //(decimal numbers are not integers) when an array of real numbers is passed to it.
+console.log(squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2])); //[ 16, 1764, 36 ]
