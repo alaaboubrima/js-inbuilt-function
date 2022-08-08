31b5b01ed851faf885a3584a4e245c6811046e47
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2022-08-08 20:02:37 +0100

    Update SquaredIntegers.js

--- SquaredIntegers.js
+++ SquaredIntegers.js
@@ -1,3 +1,5 @@
 const squareList = arr => {
   return arr.filter(num => (Number.isInteger(num) && num > 0)).map(num => Math.pow(num, 2));
 };
+//This function should return a new array containing the squares of only the positive integers 
+//(decimal numbers are not integers) when an array of real numbers is passed to it.
